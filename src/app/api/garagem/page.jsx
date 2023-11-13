69d8cde061be32049c63a0aec97be3e542395788
vruas <vitor-ruas@live.com>
2023-11-13 03:44:29 -0300

    api local + página para garagem de bikes

---
+++ src/app/api/garagem/page.jsx
@@ -0,0 +1,20 @@
+
+import { redirect } from "next/navigation";
+
+export default async function UsersView() {
+
+    let items;
+
+    try{
+        const response = await fetch('http://localhost:3000/api/garagem');
+        items = await response.json();
+        
+    }catch(error){
+        console.log(error);
+        redirect('/error');
+    }
+
+    
+
+
+  }
